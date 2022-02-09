24ba62923c1d71a05de8901ee633b942f4663a97
Udokailodigwe <ilodigweudoka@gmail.com>
2022-02-10 01:00:50 +0200

    Modified imported files function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import './css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
