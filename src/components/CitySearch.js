7d8828df1f17760066b60cc348e053f9678beb61
Udokailodigwe <ilodigweudoka@gmail.com>
2022-02-13 03:21:42 +0200

    Added state, props to citySearch component

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -4,7 +4,8 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
    state = {
       query: " ",
-      suggestions: [ ]
+      suggestions: [ ],
+      showSuggestions: undefined
    }
 
    handleInputChanged = (event) => {
@@ -20,8 +21,10 @@ class CitySearch extends Component {
 
    handleItemClicked = (suggestion) => {
       this.setState({
-         query: suggestion
+         query: suggestion,
+         showSuggestions: false
       });
+      this.props.updateEvents(suggestion);
    }
 
    render() {
@@ -32,17 +35,16 @@ class CitySearch extends Component {
                className='city'
                value = {this.state.query}
                onChange = {this.handleInputChanged}
+               onFocus={()=> {this.setState({showSuggestions: true}) }}
             />
-            <ul className='suggestions'>
+            <ul className='suggestions' style ={this.state.showSuggestions ? {}: {display: 'none'}}>
                {this.state.suggestions.map((suggestion) => (
-                  <li 
-                     key={suggestion}
-                     onClick={() => this.handleItemClicked(suggestion)} >
+                  <li key={suggestion}  onClick={() => this.handleItemClicked(suggestion)} >
                      {suggestion}
                   </li>
                ))}
-               <li key='all'>
-                  <b>See all cities</b>
+               <li onClick={() => this.handleItemClicked("all")}>
+                  <b >See all cities</b>
                </li>
             </ul>
          </div>
