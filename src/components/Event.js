b0aaf449534b51a24188cc8a5741f4242cbc5f3b
Udokailodigwe <ilodigweudoka@gmail.com>
2022-02-20 22:41:44 +0200

    Updated

--- src/components/Event.js
+++ src/components/Event.js
@@ -4,7 +4,7 @@ class Event extends Component {
    render() {
       return (
          <div>
-            <p>{{this.props.event.summary}}</p>
+            <p>{this.props.event.summary}</p>
          </div>
       );
    }
