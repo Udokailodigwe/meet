67d798c19284a41acbccd948ed49944f2a483503
Udokailodigwe <ilodigweudoka@gmail.com>
2022-02-22 14:10:22 +0200

    Updated handleinputchange function

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -3,26 +3,44 @@ import React, { Component } from 'react';
 class NumberOfEvents extends Component {
    state = {
       numberOfEvents: 32,
+      message: ' ',
    }
 
-   handleInputChange = (event) => {
-      const number = event.target.value;
-      if (number > 0 && number <=32) {
-         this.setState({
-            numberOfEvents: number,
-         });
-      } 
-   };
+
+
+handleInputChange = (event) => {
+   this.setState({
+      numberOfEvents: event.target.value,
+   });
+   this.props.updateNumberofEvents(event.target.value);
+};
+
+
+   // handleInputChange = (event) => {
+   //    const number = event.target.value;
+   //    if (number < 1 && number > 32) {
+   //       this.setState({
+   //          message: 'please choose between 1 and 32',
+   //       });
+   //       this.props.updateNumberofEvents(32);
+   //    } else {
+   //       this.setState({
+   //          numberOfEvents: number,
+   //          message: ' ',
+   //       });
+   //    }
+   //    this.props.updateNmuberOfEvents(event.target.value);
+   // };
 
    render ( ) {
       return (
          <div className='NumberOfEvents'>
             <p><b>Number of Events: </b></p>
             <input 
-            type = "number"
+            type = 'number'
             className='number-of-events'
             value = {this.state.numberOfEvents}
-            onChange = {(e) => this.handleInputChange(e)}
+            onChange = {this.handleInputChange}
             />
             </div>
       );
