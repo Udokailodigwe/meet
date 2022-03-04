c8116524ff950798c2a8e820abcdb61b1e58a1cd
Udokailodigwe <ilodigweudoka@gmail.com>
2022-03-04 06:46:30 +0200

    Added loading message while fetching events

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -9,6 +9,13 @@ class EventList extends Component {
       const {events} = this.props;
       return (
       <Row className="EventList">
+         {events.length === 0 && 
+            <div>
+               <h2>Fetching events...</h2>
+               <i>
+               (Please note, events won't display if number of event input is 0
+               </i>
+            </div>}
          {events.map((event) => (
             <Col className='col' key={event.id}> 
                <Event  event={event} />
