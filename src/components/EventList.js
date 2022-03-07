a0fc0851d5c807b0bd3b46cee6a52204264f14ab
Udokailodigwe <ilodigweudoka@gmail.com>
2022-03-07 22:21:26 +0200

    Imported warning alert from alert component and wrote logic for message presentation in render section

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Event from './Event';
+import { WarningAlert } from './Alert';
 
 import '../css/Eventlist.css';
 
-class EventList extends Component {
-   render() {
+class EventList extends Component {  
+
+render() {
       const {events} = this.props;
       return (
       <Row className="EventList">
-         {events.length < 1 && 
-            <div>
-               <h2 id= 'event_load'>Waiting for events...</h2>
-               <i  id= 'event_load'>
-               (Please note, events won't display if amount of event input is less than 1)
-               </i>
-            </div>}
-         {events.map((event) => (
+            {events.length >= 1 &&
+               <p className='warning_text'>
+               <WarningAlert  text='Most displayed events will soon be, or already outdated! '/>
+               </p>
+            }
+            {events.map((event) => (
             <Col className='col' key={event.id}> 
                <Event  event={event} />
             </Col>
