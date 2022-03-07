b5a9c7ae6dbd58e9787d7c026f8be1fcecd62b85
Udokailodigwe <ilodigweudoka@gmail.com>
2022-03-07 22:20:11 +0200

    imported Alert component, and wrote logic for alert message presentations

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../css/Citysearch.css'
+import {InfoAlert} from './Alert';
 
 
 class CitySearch extends Component {
@@ -14,10 +15,18 @@ class CitySearch extends Component {
       const suggestions = this.props.locations.filter((location) => {
          return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
       });
-      this.setState({
+      if (suggestions.length === 0) {
+         this.setState({
+            query: value,
+            infoText: 'We can not find the city you are looking for, please try another city.',
+         });
+      }else {
+         return this.setState({
          query: value,
-         suggestions
+         suggestions,
+         infoText: ' '
       });
+      }
    };
 
    handleItemClicked = (suggestion) => {
@@ -31,18 +40,24 @@ class CitySearch extends Component {
    render() {
       return (
          <div className="CitySearch">
+            <p className = 'info_alert'>
+               <InfoAlert  text={this.state.infoText} />
+            </p>
+            <p id = 'search_text'>Search for tech events around the world</p>
             <input
                type = 'text'
                className='city'
                value = {this.state.query}
                onChange = {this.handleInputChanged}
-               onFocus={()=> {this.setState({showSuggestions: true})
-               }}
-               placeholder="Search for a city"
-            />
-            <ul className='suggestions' style ={this.state.showSuggestions ? {}: {display: 'none'}}>
+               onFocus={()=> {this.setState({showSuggestions: true})}}
+               placeholder="Enter a city"
+               />
+            <ul className='suggestions' 
+                  style ={this.state.showSuggestions ? {}: {display: 'none'}}>
                {this.state.suggestions.map((suggestion) => (
-                  <li key={suggestion}  onClick={() => this.handleItemClicked(suggestion)} >
+                  <li key={suggestion}
+                        onClick={() => this.handleItemClicked(suggestion)} 
+                        style={{"display":"block"}}>
                      {suggestion}
                   </li>
                ))}
