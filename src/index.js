05157f3ca56f39783eef549ad4a8a5ccd9417079
Udokailodigwe <ilodigweudoka@gmail.com>
2022-03-08 20:17:24 +0200

    Registered service worker for PWA functionality

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import './css/index.css';
 
+import './css/index.css';
 
 import * as atatus from 'atatus-spa';
 atatus.config('723f6a6983144581931bc74f469bf666').install();
