36e5f721c9c1bbc7deaec51186b63f0874488e49
Udokailodigwe <ilodigweudoka@gmail.com>
2022-03-09 06:23:11 +0200

    Modified welcome page appearance with classes

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -21,7 +21,7 @@ function WelcomeScreen(props) {
                            <b>Sign in with google</b>
                         </button>
                         <div className="createGoogleAccount">
-            <p>Need a google account to sign in?<br></br> 
+            <p className="sign_in-text">Need a google account to sign in?<br></br> 
             Create one <a href="https://accounts.google.com/signup/v2/webcreateaccount?hl=en&flowName=GlifWebSignIn&flowEntry=SignUp">here!</a>
             </p>
             
