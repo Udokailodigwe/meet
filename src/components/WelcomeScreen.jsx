d3bcd60480e69d26c086c462abb30cd6dd3f13e3
Udokailodigwe <ilodigweudoka@gmail.com>
2022-03-09 05:57:58 +0200

    Modified welcome page appearance with classes

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -3,29 +3,30 @@ import '../css/WelcomeScreen.css';
 
 function WelcomeScreen(props) {
    return props.showWelcomeScreen ?
-      (
-         <div className="WelcomeScreen">
-            <h1>Welcome to the Meet app</h1>
+      (<div className="WelcomeScreen">
+            <h1>Welcome to the Meet Up app</h1>
                <h4>
                   Log in to see upcoming events around the world for full-stack developers
                </h4>
-                  <div className="button_cont" align="center">
-                     <div class="google-btn">
-                        <div class="google-icon-wrapper">
+                        <div class="button_cont" align="center">
                            <img
                               class="google-icon"
                               src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                               alt="Google sign-in"
                            />
-                        </div>
                         <button onClick={() => { props.getAccessToken() }}
                            rel="nofollow noopener"
                            class="btn-text"
                         >
                            <b>Sign in with google</b>
                         </button>
+                        <div className="createGoogleAccount">
+            <p>Need a google account to sign in?<br></br> 
+            Create one <a href="https://accounts.google.com/signup/v2/webcreateaccount?hl=en&flowName=GlifWebSignIn&flowEntry=SignUp">here!</a>
+            </p>
+            
+        </div>
                         </div>
-                     </div>
                      <a
                         href="https://udokailodigwe.github.io/meet/privacy.html"
                         rel="nofollow noopener"
