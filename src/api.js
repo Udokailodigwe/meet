cca7c19d9bb151b4944fff644976783f9da8e2e7
Udokailodigwe <ilodigweudoka@gmail.com>
2022-03-09 02:12:27 +0200

    Added condition to check online state of the app, to enable service worker functionality

--- src/api.js
+++ src/api.js
@@ -23,6 +23,12 @@ export const getEvents = async () => {
          NProgress.done();
          return mockData;
    }
+   if (!navigator.onLine) {
+   const events = await localStorage.getItem("lastEvents");
+   NProgress.done();
+   return events ? JSON.parse(events).events:[];
+   }
+
    const token = await getAccessToken();
       if (token) {
       removeQuery();
