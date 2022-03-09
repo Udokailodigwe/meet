c3ab1e177fb6d8116d37afdc3c2f4177fa7f5c29
Udokailodigwe <ilodigweudoka@gmail.com>
2022-03-09 03:40:56 +0200

    Created welcome page component.

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -27,7 +27,7 @@ function WelcomeScreen(props) {
                         </div>
                      </div>
                      <a
-                        href="https://YOUR_GITHUB_USERNAME.github.io/meet/privacy.html"
+                        href="https://udokailodigwe.github.io/meet/privacy.html"
                         rel="nofollow noopener"
                      >
                         Privacy policy
