86a1defc54f9ebc7b3f2fa11654b95e1e5640540
Udokailodigwe <ilodigweudoka@gmail.com>
2022-03-09 05:57:08 +0200

    Updated navigator.onLine condition for warning text on offline display

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -12,9 +12,8 @@ render() {
       return (
       <Row className="EventList">
             <p className='warning_text'>
-            {!navigator.online ?
-               (<WarningAlert  text='You are currently offline! Displayed events might be out of date!'/>) : (<WarningAlert text=' '/>)
-            }</p>
+            {!navigator.onLine ? (<WarningAlert  text='You are currently offline! Displayed events might be out of date! '/>) : (<WarningAlert text=' '/>)}
+            </p>
             {events.map((event) => (
             <Col className='col' key={event.id}> 
                <Event  event={event} />
